9b409b29cf099ab65c820d385f3ed54eda6d58cd
pratham <prathammp0007@gmail.com>
2025-03-05 08:26:45 +0000

    first commit

--- java.java
+++ java.java
@@ -3,10 +3,11 @@ class java {
             int x=121;
         String rev = "";
         String newx = String.valueOf(x);
-        for (String i : newx){
+        for (int i=newx.length() ){
           rev = rev+i;  
         }
         System.out.println(rev);
+        System.out.print("please please help helpppppp")
         }
     }
     
